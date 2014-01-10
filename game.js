e44d2ee9a05eb259d9f1860a09cdaec7205cb33c
vagrant <vagrant@lighthouse-labs>
2014-01-10 15:43:30 +0000

    added switch function and while loop

--- game.js
+++ game.js
@@ -1,13 +1,40 @@
-<SCRIPT LANGUAGE = "JavaScript">
-
 
 var lifeBalance = 3;
-var mathQuestion = Math.floor(Math.random() * 10);
+var mathQuestion = Math.floor(Math.random() * 20);
 var answer = true;
 var playerOne = prompt("What is" + mathQuestion + "?");
 var playerTwo = prompt("What is" + mathQuestion + "?");
 
-while (lifeBalance <= 3) {
+
+var question = " ";
+question += prompt("Player 1 what is: ");
+var n1 = Math.round(Math.random() * 20);
+var n2 = Math.round(Math.random() * 20);
+question += n1
+
+switch(n1)
+	case '0.333333':
+	{
+		console.log("+");
+	break;
+	} 
+	case '0.666666':
+	{
+		console.log("*");
+		break;
+	}
+	default:
+	{
+		console.log("-");
+	}
+
+
+var randomQuestion = function {
+	return Math.random() + Math.random();
+}
+
+while (lifeBalance >= 0) {
+	lifeBalance = -1
 	if (mathQuestion == answer) {
 		console.log("You are smart!");
 	} else if {
