e02fee6e751b4342ef245b087b9a8443a81667a5
vagrant <vagrant@lighthouse-labs>
2014-01-10 02:42:15 +0000

    new game outline

--- game.js
+++ game.js
@@ -1,8 +1,22 @@
 <SCRIPT LANGUAGE = "JavaScript">
 
-var playerOneChoice;
-var playerTwoChoice;
-var playerTurn = 1;
-var playerTurn = 2;
 
-num1 = console.log(math.floor
+var lifeBalance = 3;
+var mathQuestion = Math.floor(Math.random() * 10);
+var answer = true;
+var playerOne = prompt("What is" + mathQuestion + "?");
+var playerTwo = prompt("What is" + mathQuestion + "?");
+
+while (lifeBalance <= 3) {
+	if (mathQuestion == answer) {
+		console.log("You are smart!");
+	} else if {
+		(mathQuestion != answer) {
+			console.log("Not!");
+		} else {
+			lifeBalance -= 0;
+		}
+	}
+}
+ alert(lifeBalance);
+}
